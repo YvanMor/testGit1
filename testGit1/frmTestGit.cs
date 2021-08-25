a185f218931f0ca8519aa9a97c1a87e5745361bd
Yvan Morrissey <Yvan.Morrissey@cegeplevis.ca>
2021-08-25 17:02:18 -0400

    Ajoutez des fichiers projet.

---
+++ testGit1/frmTestGit.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace testGit1
+{
+    public partial class frmTestGit : Form
+    {
+        public frmTestGit()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("allo");
+        }
+    }
+}
